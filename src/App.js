37c788bb8a44d7f99d31d1d704e595076ae79f36
chamuelm <chamuelm@gmail.com>
2018-10-14 01:01:10 +0300

    Robots are loaded from jsonplaceholder rather than local list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { robots } from './robots.js';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css'
@@ -8,9 +7,18 @@ class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => {
+				return response.json();
+			}).then(users => {
+				this.setState( {robots: users});
+			})
 	}
 
 	onSearchChange = (event) => {
@@ -21,13 +29,18 @@ class App extends React.Component {
 		const filteredRobots = this.state.robots.filter(robots => {
 			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		});
-		return (
-			<div className='tc'>
-				<h1 className='f1'>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<CardList robots={ filteredRobots } />
-			</div>
-		);
+		if (this.state.robots.length === 0) {
+			return <h1>Loading...</h1>;
+		} else {
+
+			return (
+				<div className='tc'>
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange} />
+					<CardList robots={ filteredRobots } />
+				</div>
+			);
+		}
 	}
 }
 
